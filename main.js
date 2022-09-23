dd1b3827d7cc3086268b1c1271a7bfe4a231082d
campoomaria <campoomariadelosangeles@gmail.com>
2022-09-23 01:21:21 -0300

    modificacion de farmacia index y un poco de delegaciones

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        iniciosesion: false,
+        Farmacias:[], //supongo que aqui cargaremos lo que viene del back
+      //Aqui van las variables u objetos que necesitemos
       }
+    },
+    methods:{
+      //Aqui van los metodos que necesitemos utilizar
+      alerta: function () {
+        if (this.email != ' ' && this.nombrecontact != ' ' && this.tel != 0) {
+            Swal.fire(
+                '¡Tu consulta fue enviada con exito!',
+                `Pronto nos estaremos comunicando ${this.nombrecontact}!`,
+                'success'
+            )
+            this.email = ''
+            this.nombrecontact = ''
+            this.tel = ''
+        }
+    },
     }
   }).mount('#app')
 
